98a4a6ed948ebd7bf8172364a68911ba9d085f6c
Rui Batista <ruiandrebatista@gmail.com>
2017-04-27 16:46:18 +0100

    Removed integration test.

--- src/it/scala/co/enear/fs2/kafka/RawApiSpec.scala
+++ src/it/scala/co/enear/fs2/kafka/RawApiSpec.scala
@@ -15,7 +15,7 @@ import scala.concurrent.duration._
 import DefaultSerialization._
 
 class RawApiSpec(implicit executionEnv: ExecutionEnv) extends mutable.Specification with DockerKitSpotify with DockerKafkaService with DockerTestKit{
-  implicit val strategy = Strategy.fromFixedDaemonPool(16, "kafka integration")
+  implicit val strategy = Strategy.fromCachedDaemonPool("fs2")
   implicit val scheduler = Scheduler.fromFixedDaemonPool(16, "scheduler")
 
   sequential;
@@ -32,42 +32,19 @@ class RawApiSpec(implicit executionEnv: ExecutionEnv) extends mutable.Specificat
 
   "Raw API" should {
     "Doroundtrip" in {
-      val producerStream = Producer[Task, String, String, Nothing](producerSettings) { producerControl =>
+      val producerStream = Producer[Task, String, String, ProducerMetadata[Unit]](producerSettings) { producerControl =>
           Stream[Task, String]("hello")
           .map(str => ProducerMessage[String, String, Unit](new ProducerRecord(testTopic, "key", str), ()))
             .through(producerControl.send[Unit])
-          .drain
         }
 
-      val consumerStream = time.sleep_[Task](2 seconds) ++  Consumer[Task, String, String](consumerSettings).simpleStream.plainMessages(subscription)
+      val consumerStream =  Consumer[Task, String, String](consumerSettings).simpleStream.plainMessages(subscription)
         .map(_.value)
         .take(1)
 
 
       (producerStream.drain ++ consumerStream)
         .runLast.unsafeRunAsyncFuture must beEqualTo(Some("hello")).await(0, 10 seconds)
-    }
-
-    "Consume produced messages" in {
-      val count = 10000
-      val producerStream = Producer[Task, String, String, Unit](producerSettings) { producerControl =>
-          Stream.range(0, count)
-            .covary[Task]
-            .map(_.toString)
-            .map(str => new ProducerRecord(testTopic, "test", str))
-            .to(producerControl.sendSink)
-        }
-
-      val consumerStream = time.sleep_[Task](2 seconds) ++  Consumer[Task, String, String](consumerSettings).simpleStream.plainMessages(subscription)
-        .map(_.value)
-        .take(count.toLong)
-      .map(_ => 1)
-
-      val sum = for {
-        _ <- producerStream.run
-sum <- consumerStream.runFold[Int](0)(_ + _)
-      } yield sum
-      sum.unsafeRunAsyncFuture must beEqualTo(count).await(0, 30 seconds)
     }
 
 
@@ -79,7 +56,7 @@ sum <- consumerStream.runFold[Int](0)(_ + _)
           .drain
         }
 
-      val consumerStream = time.sleep_[Task](2 seconds) ++ Consumer[Task, String, String](consumerSettings.withAutoCommit(false)).simpleStream.commitableMessages(subscription)
+      val consumerStream = time.sleep_[Task](2 seconds) ++ Consumer[Task, String, String](consumerSettings.withGroupId("test3").withAutoCommit(false)).simpleStream.commitableMessages(subscription)
         .evalMap(_.commitableOffset.commit)
       .take(1)
     
